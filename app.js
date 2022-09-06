46fe2559ed77c7ea16d8827d542dfa328f8a83e5
akashVasishter <svasishter@gmail.com>
2022-09-06 10:48:17 +0530

    ex3

---
+++ app.js
@@ -0,0 +1,9 @@
+// var username = prompt("Give a username?")
+// var welcome = "welcome this is " +username
+// alert(welcome)
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+
+    console.log("clicked!");
+})
