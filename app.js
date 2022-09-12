10eeb832f1feba7d578e338a6f2a1abb52af9d59
akashVasishter <svasishter@gmail.com>
2022-09-12 14:40:02 +0530

    ex - 9 wire fetch call

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function GetTranslationURL(text) {
+    return serverURL +"?" + "text=" + text
+}
 
 function clickHandler() {
 
-    txtOutput.innerText = "Translated: " + txtInput.value;
+    var InputText = txtInput.value; //taking input
+
+    //calling server for processing
+fetch(GetTranslationURL(InputText))
+.then(response => response.json())
+.then(json => console.log(json.contents.translated))
 };
+
+
 
 btnTranslate.addEventListener("click", clickHandler);
