576953389376a5bccc357021eeec3f1dea042461
akashVasishter <svasishter@gmail.com>
2022-09-12 23:10:34 +0530

    ex-10 fun translation url input/output/calling server for processing

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-function GetTranslationURL(text) {
-    return serverURL +"?" + "text=" + text
+var translatorURL = "https://api.funtranslations.com/translate/minion.json";
+
+function GetTranslationURL(input) {
+    return translatorURL +"?" + "text=" + input
 }
 
 //error handling
 function ErrorHandler(error) {
 
     console.log("Error occured", error);
-    alert("something is wrong")
+    alert("something is wrong");
 
 }
 
@@ -21,10 +23,14 @@ function clickHandler() {
 
     var InputText = txtInput.value; //taking input
 
+
     //calling server for processing
 fetch(GetTranslationURL(InputText))
 .then(response => response.json())
-.then(json => console.log(json.contents.translated))
+.then(json => {
+    var translatedText = json.contents.translated;
+    txtOutput.innerText = translatedText;
+})
 .catch(ErrorHandler)
 
 };
